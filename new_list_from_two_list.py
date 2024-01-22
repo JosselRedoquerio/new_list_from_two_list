a2aeee881e7d51c3e8791d665857495fd5991232
Jossel Redoquerio <josselgredoquerio@iskolarngbayan.pup.edu.ph>
2024-01-22 23:54:08 +0800

    first list

--- new_list_from_two_list.py
+++ new_list_from_two_list.py
@@ -4,3 +4,13 @@
 # Given two list of numbers, 
 # write a program to create a new list such that the new list should contain odd numbers from the first list
 # even numbers from the second list.
+
+def merge_list(list1, list2):
+    result_list = []
+    
+    # iterate first list
+    for num in list1:
+        if num % 2 != 0:
+            result_list.append(num)
+    
+
